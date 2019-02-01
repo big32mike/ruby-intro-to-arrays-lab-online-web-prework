106ff746edd36d84ac265ba7bffe16a21f17ce43
michael white <michaeljwhite@gmail.com>
2019-02-01 08:10:49 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  arr = []
+end
+
+def array_with_two_elements
+  arr = ["one", "two"]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
